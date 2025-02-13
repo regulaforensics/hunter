a49ea5d29ab87e397a1ac0a6c8bae4d9128d78e4
aliaksandr.anishchuk <aliaksandr.anishchuk@regulaforensics.com>
2025-02-13 23:20:58 +0300

    Added version 1.9.6

--- cmake/projects/jsoncpp/hunter.cmake
+++ cmake/projects/jsoncpp/hunter.cmake
@@ -15,6 +15,17 @@ hunter_add_version(
     PACKAGE_NAME
     jsoncpp
     VERSION
+    "1.9.6"
+    URL
+    "https://github.com/open-source-parsers/jsoncpp/archive/1.9.6.tar.gz"
+    SHA1
+    fb695d42da4a60e1d25d0757eff8308827f07240
+)
+
+hunter_add_version(
+    PACKAGE_NAME
+    jsoncpp
+    VERSION
     "1.9.5-b1"
     URL
     "https://github.com/julianoes/jsoncpp/archive/refs/tags/1.9.5-b1.tar.gz"
