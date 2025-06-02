8e221f79293905ec9bc7ecd1fd3e4ef76d878ac7
Ronny Nowak <dargun@gmx.net>
2025-06-02 08:00:50 +0200

    update msgpack to v4.1.3 (#644)
    * update msgpack to v4.1.3
    * update docu and example after review
    * Update docs/packages/pkg/msgpack.rst
    
    ---------
    
    Co-authored-by: NeroBurner <pyro4hell@gmail.com>

--- cmake/projects/msgpack/hunter.cmake
+++ cmake/projects/msgpack/hunter.cmake
@@ -2,6 +2,7 @@
 
 include(hunter_add_version)
 include(hunter_cacheable)
+include(hunter_cmake_args)
 include(hunter_download)
 include(hunter_pick_scheme)
 
@@ -27,6 +28,23 @@ hunter_add_version(
   eafdcb4c0a29d7416248740bd8132316a4b03910
   )
 
+hunter_add_version(
+  PACKAGE_NAME
+  msgpack
+  VERSION
+  "4.1.3"
+  URL
+  "https://github.com/msgpack/msgpack-c/archive/refs/tags/cpp-4.1.3.tar.gz"
+  SHA1
+  e53ea01821cdf8e7d0d9d56d9c2496bda59636e1
+  )
+
+  
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(msgpack CMAKE_ARGS
+  MSGPACK_USE_BOOST=OFF
+  MSGPACK_BUILD_DOCS=OFF)
+
 hunter_cacheable(msgpack)
 hunter_download(PACKAGE_NAME msgpack)
